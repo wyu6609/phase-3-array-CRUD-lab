a598c625a88185530189e3b8e2e2274213e333c9
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-03-22 09:14:23 -0400

    commit tree cb1c7495a73cc3c4b2e85b58e94fbbd981d19ce2

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,21 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  [el_1, el_2, el_3, el_4]
 end
 
-def add_element_to_end_of_array(array, element)
-  
-end
+def add_element_to_end_of_array(array, element); end
 
-def add_element_to_start_of_array(array, element)
-  
-end
+def add_element_to_start_of_array(array, element); end
 
-def remove_element_from_end_of_array(array)
-  
-end
+def remove_element_from_end_of_array(array); end
 
-def remove_element_from_start_of_array(array)
-  
-end
+def remove_element_from_start_of_array(array); end
 
-def retrieve_element_from_index(array, index_number)
-  
-end
+def retrieve_element_from_index(array, index_number); end
 
-def retrieve_first_element_from_array(array)
-  
-end
+def retrieve_first_element_from_array(array); end
 
-def retrieve_last_element_from_array(array)
-  
-end
+def retrieve_last_element_from_array(array); end
